7f5abd2831460edd3e08fe92051dc956f234d486
GabrielOchieng <ogingagabriel@gmail.com>
2024-04-11 17:13:25 +0300

    crud functionality setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
+import { Query } from "mongoose";
+import Clients from "./components/Clients";
 import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: cache,
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 
 const app = express();
 connectDB();
+app.use(cors());
 
 // Connect to DB
 
